610826347a135d31e3e27890df9f038ae38f9d96
Kx1kin <142590045+Kx1kin@users.noreply.github.com>
2023-08-21 18:24:37 +1000

    1

--- first/main.py
+++ first/main.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
->>>>>>> parent of ef4421d (init)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
