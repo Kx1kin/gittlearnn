71a391288b814584a73716eb47e22777f9f12571
Kx1kin <142590045+Kx1kin@users.noreply.github.com>
2023-08-21 18:06:56 +1000

    добавлен ризез

--- first/main.py
+++ first/main.py
@@ -10,9 +10,13 @@ def print_hi(name):
 
 def bob():
     print('bom')
+
+def rizez():
+    print('rzc')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     bob()
+    rizez()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
