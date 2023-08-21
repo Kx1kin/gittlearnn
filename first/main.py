768bc41cd2cc1861cf2f5e32d3d3462184ff1a5d
Kx1kin <142590045+Kx1kin@users.noreply.github.com>
2023-08-21 18:13:02 +1000

    добавлен ризез

--- first/main.py
+++ first/main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+<<<<<<< HEAD
 def bob():
     print('bom')
 
@@ -18,5 +19,11 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     bob()
     rizez()
+=======
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+>>>>>>> parent of ef4421d (init)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
